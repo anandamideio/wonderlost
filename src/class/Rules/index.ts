92e50899441bd52049a0d0d2521453ebf29da753
Abourass <Abourassa@AssetVal.com>
2024-09-16 23:37:17 -0600

    Refactor TomeRules class to use a generic type for settings

---
+++ src/class/Rules/index.ts
@@ -0,0 +1,101 @@
+
+abstract class Rule {
+  public name: string;
+  public label?: string;
+  public icon?: string;
+  public restricted?: boolean;
+  public onChange?: (value: any) => void | Promise<void>;
+  public requiresReload?: boolean;
+
+  constructor({ name, label, icon, restricted, onChange, requiresReload }: Rule) {
+    this.name = name;
+    this.label = label;
+    this.icon = icon;
+    this.restricted = restricted;
+    this.onChange = onChange;
+    this.requiresReload = requiresReload;
+  }
+}
+
+export class NumberRule extends Rule {
+  public type = Number;
+  public range?: { min?: number; max?: number, step?: number };
+  public defaultValue?: number;
+
+  constructor({ name, label, icon, onChange, restricted, range, defaultValue }: Omit<NumberRule, 'type'>) {
+    super({ name, label, icon, onChange, restricted });
+    this.range = range;
+    this.defaultValue = defaultValue;
+  }
+}
+
+export const isNumberRule = (rule: Rules): rule is NumberRule => rule.type instanceof Number;
+
+export class BooleanRule extends Rule {
+  public type = Boolean;
+  public defaultValue?: boolean;
+
+  constructor({ name, label, icon, onChange, restricted, defaultValue }: Omit<BooleanRule, 'type'>) {
+    super({ name, label, icon, onChange, restricted });
+    this.defaultValue = defaultValue;
+  }
+}
+
+export const isBooleanRule = (rule: Rules): rule is BooleanRule => rule.type instanceof Boolean;
+
+export class StringRule extends Rule {
+  public type = String;
+  public defaultValue?: string;
+  public filePicker?: "any" | "image" | "video" | "audio" | "document";
+  public choices?: { [key: string]: string };
+
+  constructor({ name, label, icon, onChange, choices, restricted, defaultValue, filePicker }: Omit<StringRule, 'type'>) {
+    super({ name, label, icon, onChange, restricted });
+    this.defaultValue = defaultValue;
+    this.filePicker = filePicker;
+    this.choices = choices;
+  }
+}
+
+export const isStringRule = (rule: Rules): rule is StringRule => rule.type instanceof String;
+
+export type Rules = NumberRule | BooleanRule | StringRule;
+
+export class TomeRules {
+  private GlobalSettings: Array<Rules> = [];
+  private ClientSettings: Array<Rules> = [];
+
+  public get globalSettings() {
+    return this.GlobalSettings;
+  }
+
+  public get clientSettings() {
+    return this.ClientSettings;
+  }
+
+  public addGlobal(newRule: Rules) {
+    switch (newRule.type) {
+      case Number:
+        this.GlobalSettings.push(new NumberRule(newRule as NumberRule));
+        break;
+      case Boolean:
+        this.GlobalSettings.push(new BooleanRule(newRule as BooleanRule));
+        break;
+      case String:
+        this.GlobalSettings.push(new StringRule(newRule as StringRule));
+        break;
+      default:
+        throw new Error(`Unsupported rule type: ${newRule.type}`);
+    }
+  }
+
+  constructor(pSettings?: {
+    globalSettings: Array<NumberRule | BooleanRule | StringRule>;
+    clientSettings: Array<Rules>;
+  }) {
+    if (pSettings) {
+      this.GlobalSettings = [...pSettings.globalSettings];
+      this.ClientSettings = [...pSettings.clientSettings];
+    }
+  }
+}
--- src/submodules/toasted/Toasted.ts
+++ src/submodules/toasted/Toasted.ts
@@ -1,29 +1,48 @@
-import { El } from '@magik_io/mote';
-import { Tome, TomeRules } from '../../class/Tome';
+import { El, Mote } from '@magik_io/mote';
+import { Tome } from '../../class/Tome';
+import { TomeRules } from '../../class/Rules';
 
-class Toasted extends Tome {
-  public toasts: string[];
-  public settings = new TomeRules({
-    globalSettings: [],
-    clientSettings: [{
-      name: 'toastDuration',
-      type: Number,
-      defaultValue: 3000,
-      range: {
-        min: 1000,
-        max: 10000,
-        step: 250,
-      },
-      icon: 'clock',
-      restricted: false,
-    }]
-  });
+export class Toasted extends Tome {
+  public toasts: string[] = [];
 
   constructor() {
-    super({ moduleName: 'Toasted', moduleDescription: 'A simple toast notification system' });
-    this.toasts = [];
+    super({
+      moduleName: 'Toasted',
+      moduleDescription: 'A simple toast notification system',
+      settings: new TomeRules({
+        globalSettings: [],
+        clientSettings: [{
+          name: 'toastDuration',
+          label: 'How long would you like a message to stay on screen?',
+          type: Number,
+          defaultValue: 3000,
+          range: { min: 1000, max: 10000, step: 250 },
+          icon: 'clock',
+          restricted: false,
+        }]
+      }),
+      hooks: new Map([
+        ['renderChatLog', async (app, html) => {
+          if (document.body.classList.contains('stream')) return;
+          const chatTab = html[0];
 
+          new Mote(
+            html[0].querySelector('#chat-log')!.cloneNode(false) as unknown as `div#${string}`
+          ).addClass(this.moduleName)
+            .on('click', (ec) => handleMoustEvent(ev))
+            .on('contextmenu', (ev) => { handleMouseEvent(ev) })
+            .appendToBody()
 
+          Hooks.on('renderChatMessage', (app, html, options) => {
+            if (chatTab.classList.contains('active') && !chatTab.closest('#sidebar').classList.contains('collapsed')) return;
+            const newNode = html[0].cloneNode(true);
+            addMessage(newNode);
+          });
+        }],
+      ])
+    });
+
+    this.registerRules();
   }
 
   addToast(message) {
@@ -31,8 +50,18 @@ class Toasted extends Tome {
     console.log(`Toast added: ${message}`);
   }
 
-  showToasts() {
+  public showToasts() {
     console.log('Current toasts:', this.toasts);
+  }
+
+  public removeMessage(node, { time = 0.3, delay = fadeOutDelay } = {}) {
+    if (fadeOutDelay < 0) return;
+
+    TweenMax.to(node, time, {
+      opacity: 0, height: 0, delay, onComplete: () => {
+        node.remove();
+      }
+    });
   }
 
   static expandSidebarInstant(sidebar: HTMLDivElement) {
@@ -41,7 +70,7 @@ class Toasted extends Tome {
     const sideB = new El<'div', true>(sidebar).removeClass('collapsed').unset(['width', 'height']);
     ui.sidebar._collapsed = false;
 
-    const icon = sideB.element.querySelector('#sidebar-tabs a.collapse i');
+    const icon = sideB.element.querySelector('#sidebar-tabs a.collapse i') as HTMLDivElement;
     if (!icon) throw new Error('[Toasted:expandSidebarInstant] -> Error: Icon element was not found');
 
     new El(icon).removeClass('fa-caret-left').addClass('fa-caret-right');
@@ -49,9 +78,9 @@ class Toasted extends Tome {
     Hooks.callAll('sidebarCollapse', ui.sidebar, ui.sidebar._collapsed)
   }
 
-  static findTarget(card: HTMLDivElement, event, messageID) {
+  static findTarget(card: HTMLDivElement, event, messageID: string) {
     const cardRect = card.getBoundingClientRect();
-    const popupRect = document.querySelector(`.${moduleName}`)!.getBoundingClientRect();
+    const popupRect = document.querySelector(`.${this.name}`)!.getBoundingClientRect();
     let x = event.clientX - popupRect.left + cardRect.left,
       y = event.clientY - popupRect.top + cardRect.top;
 
